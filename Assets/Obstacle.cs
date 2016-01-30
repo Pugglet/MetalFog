c6d54c11b6c748eed9b1f0a9393273e5f647db02
Radnom-g <radnomgames@gmail.com>
2016-01-30 21:17:13 +1300

    * Looping obstacles, also faster

--- Assets/Obstacle.cs
+++ Assets/Obstacle.cs
@@ -17,6 +17,11 @@ public class Obstacle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position += new Vector3(0, YSpeed, 0) * Time.deltaTime;
+
+		if (transform.position.y > 10) {
+			Vector3 newPos = new Vector3(0, -40, 0);
+			transform.position = newPos;
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
