53eacf3bd2db4cf15ea9ea7f97c47dcec5b2b22a
ladiespls <fsta657@aucklanduni.ac.nz>
2016-01-31 15:07:39 +1300

    * Added random scound script

---
+++ Assets/BackgroundMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    [SerializeField]
+    AudioClip[] clip;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        int clipCount = clip.Length;
+        int AynRand = Random.Range(0, clipCount);
+
+        AudioSource source = GetComponent<AudioSource>();
+        source.clip = clip[AynRand];
+        source.Play();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
